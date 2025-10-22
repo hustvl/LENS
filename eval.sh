a745e3413a4ba0994b1927918e2f36e552a44da4
斌 <80621710+catchcodes@users.noreply.github.com>
2025-10-22 22:25:58 +0800

    change weights path

--- eval.sh
+++ eval.sh
@@ -1,31 +1,23 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-# stage1 
-torchrun --standalone --nproc_per_node 1 eval/evaluate_res.py \
-    --model_path './weights/stage1/qwen2p5_87p5' \
-    --image_dir './datasets' \
-    --if_stage1 
-
-
-# stage2
 # refercoco series
 torchrun --standalone --nproc_per_node 1 eval/evaluate_res2.py \
-    --model_path './weights/stage2/qwen2p5_refcoco' \
+    --model_path './pretrained/qwen2p5_refcoco' \
     --image_dir './datasets'
 
 # reasonseg
 torchrun --standalone --nproc_per_node 1 eval/evaluate_reasonseg.py \
-    --model_path './weights/stage2/qwen2p5_reasonseg' \
+    --model_path '/path/to/qwen2p5_reasonseg' \
     --image_dir './datasets'
 
 # reasonseg cot
 torchrun --standalone --nproc_per_node 1 eval/evaluate_reasonseg.py \
-    --model_path './weights/stage2/qwen2p5_reasonseg' \
+    --model_path '/path/to/qwen2p5_reasonseg_cot' \
     --image_dir './datasets' \
     --cot
 
 # groundingsuite-eval
 torchrun --standalone --nproc_per_node 1 eval/evaluate_res2_gs.py \
-    --model_path './weights/stage2/qwen2p5_reasonseg' \
+    --model_path '/path/to/qwen2p5_reasonseg' \
     --image_dir './datasets/coco/unlabeled2017' \
     --anno_path './datasets/GroundingSuite-Eval.jsonl'
